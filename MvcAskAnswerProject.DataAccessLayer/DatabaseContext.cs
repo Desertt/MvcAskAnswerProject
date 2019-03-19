1d9964b488a490083df22a24a23d5e90d89f09b1
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-03-20 02:16:33 +0300

    Create Database With Code First

---
+++ MvcAskAnswerProject.DataAccessLayer/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using MvcAskAnswerProject.Entities;
+using System.Data.Entity;
+
+namespace MvcAskAnswerProject.DataAccessLayer
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Kullanici> Kullanicilar { get; set; }
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Soru> Sorular { get; set; }
+        public DbSet<Cevap> Cevaplar { get; set; }
+
+        public DatabaseContext()
+        {
+
+        }
+
+
+    }
+}
--- MvcAskAnswerProject.UI/Controllers/HomeController.cs
+++ MvcAskAnswerProject.UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcAskAnswerProject.DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,8 @@ namespace MvcAskAnswerProject.UI.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            DatabaseContext ct = new DatabaseContext();
+            ct.Database.CreateIfNotExists();
             return View();
         }
 
