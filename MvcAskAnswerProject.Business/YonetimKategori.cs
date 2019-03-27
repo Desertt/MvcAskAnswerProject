1240bf3053d15325d0194c204f7eba42a904361b
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-03-28 00:29:38 +0300

    Add New Category

---
+++ MvcAskAnswerProject.Business/YonetimKategori.cs
@@ -0,0 +1,14 @@
+﻿using MvcAskAnswerProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcAskAnswerProject.Business
+{
+    public class YonetimKategori:YonetimBase<Kategori>
+    {
+        
+    }
+}
---
+++ MvcAskAnswerProject.UI/Controllers/KategoriAdminController.cs
@@ -0,0 +1,86 @@
+﻿using MvcAskAnswerProject.Business;
+using MvcAskAnswerProject.Entities;
+using System.Web.Mvc;
+
+namespace MvcAskAnswerProject.UI.Controllers
+{
+    public class KategoriAdminController : Controller
+    {
+        private YonetimKategori yonetimKategori = new YonetimKategori();
+        // GET: KategoriAdmin
+        public ActionResult Index()
+        {
+            //return View(db.Kategoris.ToList());
+            return View(yonetimKategori.List());
+        }
+
+
+        // GET: KategoriAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                yonetimKategori.Insert(kategori);
+                return RedirectToAction("Index");
+            }
+
+            return View(kategori);
+          
+        }
+
+        // GET: KategoriAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            /*if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategoris.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);*/
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Kategori kategori)
+        {
+            /*if (ModelState.IsValid)
+            {
+                db.Entry(kategori).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategori);*/
+            return View();
+        }
+
+        // GET: KategoriAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            /*if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategoris.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);*/
+            return View();
+        }
+
+    }
+}
